196017895ec96a2dd1fb6287d4dee31210190640
idealclaritysolutions <idealclaritysolutions@gmail.com>
2026-06-18 02:56:32 -0500

    Fix TypeScript types in ThemeProvider component

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,21 +1,11 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function ThemeProvider({
   children,
   ...props
-}: {
-  children: React.ReactNode
-  attribute?: string | string[]
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
-  storageKey?: string
-  themes?: string[]
-  forcedTheme?: string
-  enableColorScheme?: boolean
-}) {
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
