dd518083942b75357fa2cbc1fd26245b6ec1fe8e
idealclaritysolutions <idealclaritysolutions@gmail.com>
2026-06-18 02:58:48 -0500

    Update ThemeProvider props type to 'any'
    Changed the type of props in ThemeProvider to 'any'.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,9 +3,6 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
