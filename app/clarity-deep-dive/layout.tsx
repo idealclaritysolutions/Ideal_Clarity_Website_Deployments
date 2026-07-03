8908d601d817a4ade7d2a14e998c66265e27963d
idealclaritysolutions <idealclaritysolutions@gmail.com>
2026-07-03 00:38:17 -0500

    Integrate Facebook Pixel tracking in layout
    Added Facebook Pixel tracking script to layout component.

--- app/clarity-deep-dive/layout.tsx
+++ app/clarity-deep-dive/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "The Clarity Intensive | Ideal Clarity",
@@ -11,5 +12,32 @@ export default function ClarityIntensiveLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', 'YOUR_PIXEL_ID_HERE');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID_HERE&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {children}
+    </>
+  );
 }
